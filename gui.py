4a1650bcbfd3c994935fb27c855dbfa5c905fcf3
Pablo Monardo <pablomonardo@gmail.com>
2022-12-09 17:54:57 -0300

    Update gui.py
    Started to create the UI

--- gui.py
+++ gui.py
@@ -1,2 +1,46 @@
 import tkinter as tk
+from tkinter import ttk
+from PIL import ImageTk
 
+def clear_window(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+def load_main():
+    login_frame.grid(row=3, column=0, sticky="nesw")
+    login_frame.tkraise()
+
+    #Logo
+    logo_img = ImageTk.PhotoImage(file=r"C:\Users\User\Desktop\BurgerShop\assets\logo.png")
+    logo_widget = tk.Label(login_frame, image= logo_img)
+    logo_widget.image = logo_img
+    logo_widget.pack()
+
+    ttk.Entry(login_frame, textvariable="User", width= 40).pack(pady=40)
+    ttk.Entry(login_frame, textvariable="Password", width= 40).pack()
+
+
+    tk.Button(
+        login_frame,
+        text="Login",
+        bg="red",
+        fg="white",
+        cursor="hand2"
+    ).pack(pady=20)
+    
+
+def load_vendorsUI():
+    pass
+
+def load_adminUI():
+    pass
+
+
+root = tk.Tk()
+root.title("Pabletoo's Burger")
+login_frame = tk.Frame(root, width= 800, height=600)
+vendors_frame = tk.Frame(root, width= 800, height=600)
+admin_frame = tk.Frame(root, width= 800, height=600)
+
+load_main()
+root.mainloop()
