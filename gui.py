2ef82a1683f56beae0c9ae46ba383b339e59e473
Pablo Monardo <pablomonardo@gmail.com>
2022-12-09 22:09:17 -0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ def clear_window(frame):
         widget.destroy()
 
 def load_main():
-    login_frame.grid(row=3, column=0, sticky="nesw")
+    login_frame.grid(row=2, column=0, sticky="nesw")
     login_frame.tkraise()
 
     #Logo
@@ -31,7 +31,31 @@ def load_main():
     
 
 def load_vendorsUI():
-    pass
+    vendors_frame.grid(row= 2, column= 2, sticky="nesw")
+    vendors_frame.tkraise()
+
+    logo_img = ImageTk.PhotoImage(file=r"C:\Users\User\Desktop\BurgerShop\assets\logo.png")
+    logo_widget = tk.Label(vendors_frame, image= logo_img)
+    logo_widget.image = logo_img
+    logo_widget.grid(row=0, column=0)
+    logo_widget.pack()
+
+    container = tk.Frame(vendors_frame, width=60)
+    left_side = tk.Label(container, text="Hello there").pack()
+    right_side = tk.Label(container, text="this is another hello there").pack()
+    
+    container.pack()
+
+
+    cancel_button = tk.Button(vendors_frame, text="Cancel")
+    cancel_button.pack()
+
+    submit_button = tk.Button(vendors_frame, text="Submit")
+    submit_button.pack()
+
+    logout_button = tk.Button(vendors_frame, text="Logout", bg="red")
+    logout_button.pack()
+
 
 def load_adminUI():
     pass
@@ -43,5 +67,5 @@ login_frame = tk.Frame(root, width= 800, height=600)
 vendors_frame = tk.Frame(root, width= 800, height=600)
 admin_frame = tk.Frame(root, width= 800, height=600)
 
-load_main()
+load_vendorsUI()
 root.mainloop()
