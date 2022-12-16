f7c1b9e957c3f3fcfb721de5ef2f18eb58d254bb
Pablo Monardo <pablomonardo@gmail.com>
2022-12-16 16:30:35 -0300

    Create db_commands.sql

---
+++ db_commands.sql
@@ -0,0 +1,16 @@
+INSERT INTO Products VALUES
+	(1,"Cheeseburger", 1, 1.65, 60,0),
+	(2,"Doble Cheeseburger", 1, 3.30, 60,0),
+	(3,"Triple Cheeseburger", 1, 4.30, 60,0), 
+	(4,"Menu4u Simple", 1, 3.30, 60,0),
+	(5,"Menu4u Medium", 1, 4.30, 60,0),
+	(7,"Menu4u Triple", 1, 5.50, 60,0),
+	(8,"SuperMenu w/chiken", 1, 8.05, 100,0),
+	(9,"SuperMenu w/fries", 1, 11.4, 100,0),
+	(10,"Coca-Cola small", 3, 2.25, 100,0),
+	(11,"Aquarius small", 3, 2.25, 100,0), 
+	(12,"Water 50cl.", 3, 2.05, 100,0),
+	(13,"IceCream", 2, 3.5, 100,0),
+	(14,"Mini IceCream", 2, 1.75, 100,0),
+	(15,"Oreo IceCream", 2, 1.9, 100,0);
+	
